64c1d71944831dafdc8b5fd30d36a19868c0f6b9
Valentin Garcia <d3loygamer@gmail.com>
2022-09-07 11:25:19 -0300

    Navbar design

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import "./NavBar.css"
+
+
+function Navegacion(){
+return(
+<div className="">
+    <nav className="navbar navbar-expand-lg navbar-light navBackground">
+        <div className="container-fluid">
+            <a href="#" className="navbar-brand">
+            <img className="imgLogo" src={require("./f1logo.png")} alt="Formula 1 Shop"/>
+            </a>
+            <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+                <div className="navbar-nav">
+                    <a href="#" className="nav-item nav-link color" >Home</a>
+                    <a href="#" className="nav-item nav-link color">Remeras</a>
+                    <a href="#" className="nav-item nav-link color">Gorras</a>
+                    <a href="#" className="nav-item nav-link color">Buzos</a>
+                    <a href="#" className="nav-item nav-link color">Cascos</a>
+                    <a href="#" className="nav-item nav-link color">Cuadros</a>
+                </div>
+                <div className="navbar-nav ms-auto">
+                    <a href="#" className="nav-item nav-link color">Carrito</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</div>
+)
+}
+export default Navegacion;
